2e1123a6412466efd70be8fccae61eee1ac2a7b0
afraefatuaki <afrae@elfatuaki.com>
2021-06-13 20:36:01 +0200

    update

---
+++ assets/js/game.js
@@ -0,0 +1,30 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (human, computer, target) => {
+    if (Math.abs(human - target) <= Math.abs(computer - target)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const updateScore = (winner) => {
+    if (winner = 'human') {
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+
+};
+
+const advanceRound = () => {
+    currentRoundNumber++;
+};
